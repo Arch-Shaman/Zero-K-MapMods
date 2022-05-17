a9bcdcf59fbce5d5572fbb961816a9e6a339ec49
Tony M <48730089+Arch-Shaman@users.noreply.github.com>
2022-05-17 18:33:17 -0400

    Whitespace + rewrite

--- Finnish Flag/LuaRules/Gadgets/workaround.lua
+++ Finnish Flag/LuaRules/Gadgets/workaround.lua
@@ -1,36 +1,24 @@
-function gadget:GetInfo()
-  return {
-    name      = "Workaround for Hovers",
-    desc      = "Gadget I took off the internet to bypass water damage in later spring versions. Precautionary measures.",
-    author    = "Kloot made it I think, I just copy pasted",
-    date      = "Idk..",
-    license   = "I took it off the internet",
-    layer     = -3,
-    enabled   = true
-  }
+if not (gadgetHandler:IsSyncedCode()) then
+	return
 end
-	
+function gadget:GetInfo()
+	return {
+		name      = "Workaround for Hovers",
+		desc      = "Bypass water damage for hovers.",
+		author    = "Shaman based on Kloot's original gadget",
+		date      = "November 2013",
+		license   = "CC-0",
+		layer     = -3,
+		enabled   = true
+	}
+end
+local DAMAGE_EXTSOURCE_WATER = -5
 
-	
-
-    if (gadgetHandler:IsSyncedCode()) then
-      local DAMAGE_EXTSOURCE_WATER = -5
-     
-      function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID)
-        if (weaponDefID ~= DAMAGE_EXTSOURCE_WATER) then
-           -- not water damage, do not modify
-           return damage, 1.0
-        end
-     
-        local unitDef = UnitDefs[unitDefID]
-        local moveDef = unitDef.moveDef
-     
-        if (moveDef == nil or moveDef.family ~= "hover") then
-          -- not a hovercraft, do not modify
-          return damage, 1.0
-        end
-     
-        return 0.0, 1.0
-      end
-    end
-
+function gadget:UnitPreDamaged(unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID)
+	local moveDef = UnitDefs[unitDefID].moveDef
+	if weaponDefID ~= DAMAGE_EXTSOURCE_WATER or (moveDef == nil or moveDef.family ~= "hover") then
+		return damage, 1.0 -- not water damage or not a hovercraft, do not modify
+	else
+		return 0.0, 1.0
+	end
+end
